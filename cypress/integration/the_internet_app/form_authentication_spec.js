cf1c78d6a40d82ee60b451ba7623c5f4e4502b08
Mehmet Ali Seçgin <mesecg@ttu.ee>
2021-11-13 18:26:33 +0300

    Initial commit

---
+++ cypress/integration/the_internet_app/form_authentication_spec.js
@@ -0,0 +1,14 @@
+describe("Form authentication page", () => {
+    beforeEach(() => {
+        cy.visit("/login");
+    });
+
+    it('should login to secure area with valid credentials', function () {
+        cy.get('input[name=username]').type("tomsmith")
+
+        // {enter} causes the form to submit
+        cy.get('input[name=password]').type(`SuperSecretPassword!{enter}`)
+
+        cy.get(".flash.success").should("be.visible");
+    });
+});
