01a0641ba433873d316e6e352a278e5075a540e1
Fothul Karim Forhan <fothulkforhan@gmail.com>
2021-11-13 19:23:38 +0200

    Adding test for Add/Remove elements!!

---
+++ cypress/integration/the_internet_app/add_remove_elements_spec.js
@@ -0,0 +1,27 @@
+describe("Add/Remove Elements Page", () => {
+    before(() => {
+        cy.visit("add_remove_elements/");
+    });
+
+    it('should add element', function () {
+        cy.get('div[class=example]').find('button').click();
+
+        cy.get('#elements .added-manually').should("have.length",1)
+    });
+
+    it('should delete element', function () {
+        cy.get('#elements .added-manually').click().should("have.length",0)
+
+    });
+
+    it('should add 2 elements and delete one of them', function () {
+        cy.get('div[class=example]').find('button').click().click();
+
+
+    });
+    it('should delete one of the elements', function () {
+
+        cy.get('#elements .added-manually').should("have.length",2).first().click();
+        cy.get('#elements .added-manually').should("have.length",1);
+    });
+});
